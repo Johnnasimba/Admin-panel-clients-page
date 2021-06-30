b879c2180c58aa222b0a244e95e669b437af0214
JOHN NASIMBA <nasimba4john@gmail.com>
2021-06-30 20:51:47 +0200

    Creating client-list component and it's styles

--- src/components/clients-list/client-list.component.jsx
+++ src/components/clients-list/client-list.component.jsx
@@ -12,8 +12,9 @@ const ClientsList = ()  => {
                 <input type="text" placeholder="Search" />
             </div>
             <div className="client-list" >
+               {/* client */}
                 <div className="single-client">
-                    <img src={process.env.PUBLIC_URL + '/images/john nasimba.jpg'} alt="admin" />
+                    <img src={process.env.PUBLIC_URL + '/images/melissa adam.jpg'} alt="Melissa Adam" />
                     <div className="client-name-and-email">
                         <div className="client-name">
                              Melissa Adam 
@@ -24,6 +25,59 @@ const ClientsList = ()  => {
 
                     </div>
                 </div>
+               {/* client */}
+                <div className="single-client romana-francois">
+                    <img src={process.env.PUBLIC_URL + '/images/romana francois(small).jpg'} alt="Romana Francois" />
+                    <div className="client-name-and-email">
+                        <div className="client-name">
+                             Romana Francois 
+                        </div>
+                        <div className="client-email">
+                            romancefrancois@example.com
+                        </div>
+
+                    </div>
+                </div>
+               {/* client */}
+                <div className="single-client">
+                    <img src={process.env.PUBLIC_URL + '/images/james robert.jpg'} alt="James Robert" />
+                    <div className="client-name-and-email">
+                        <div className="client-name">
+                             James Robert
+                        </div>
+                        <div className="client-email">
+                            robertjames@gmail.com
+                        </div>
+
+                    </div>
+                </div>
+               {/* client */}
+                <div className="single-client ">
+                    <img src={process.env.PUBLIC_URL + '/images/nancy williams.jpg'} alt="Nancy Williams" />
+                    <div className="client-name-and-email">
+                        <div className="client-name">
+                             Nancy Williams 
+                        </div>
+                        <div className="client-email">
+                           nancywilliams95@outlook.com
+                        </div>
+
+                    </div>
+                </div>
+               {/* client */}
+                <div className="single-client">
+                    <img src={process.env.PUBLIC_URL + '/images/mueen agherdien.jpg'} alt="Melissa Adam" />
+                    <div className="client-name-and-email">
+                        <div className="client-name">
+                            Mueen Smith
+                        </div>
+                        <div className="client-email">
+                            adammelissa@gmail.com
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </div>
     )
