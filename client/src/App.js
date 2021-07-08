fa972e194265b4d92689c8611d6159f26ae2544e
JOHN NASIMBA <nasimba4john@gmail.com>
2021-07-08 11:14:12 +0200

    Removing the https protocol, the localhost domain, and port since we are using process.env.port on the server

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,7 @@ const App = () => {
 
   const getData=()=>{
 
-    fetch('http://localhost:5000/client'
-    ,{
+    fetch('/client', {
       headers : { 
         'Content-Type': 'application/json',
         'Accept': 'application/json'
