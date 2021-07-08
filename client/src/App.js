f4279fdd9f71df537cfa12302900bc69ef30fb7c
JOHN NASIMBA <nasimba4john@gmail.com>
2021-07-08 11:00:44 +0200

    Trying to resolve bug which is causing the front end unable to fetch data from the server

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   const getData=()=>{
 
-    fetch(`${process.env.API_URL}/client`
+    fetch('http://localhost:5000/client'
     ,{
       headers : { 
         'Content-Type': 'application/json',
